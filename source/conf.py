483bf419638c8686368e7e5a43545c4e5c893891
Masafumi NAKANE <max@freee.co.jp>
2023-07-14 19:46:50 +0900

    conf.py: Ver. 202307.0 (2023-07-18)

--- source/conf.py
+++ source/conf.py
@@ -20,9 +20,9 @@ import re
 
 project = 'freeeアクセシビリティー・ガイドライン'
 author = 'freee株式会社'
-version = 'Ver. 202306.1'
+version = 'Ver. 202307.0'
 release = version
-publishedDate = u'2023年6月16日'
+publishedDate = u'2023年7月18日'
 copyright = '2020-{pubYear}, freee株式会社'.format(
   pubYear = re.search(r'^(\d{4})年', publishedDate).group(1)
 )
