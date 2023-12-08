cf113d36124eaa6a0f5182df2bca3787dbc31c5c
Masafumi NAKANE <max@freee.co.jp>
2023-12-08 17:05:05 +0900

    commit時チェックにfaqのスキーマとYAMLを追加

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,7 @@
 export default {
   'data/json/schemas/common.json': (files) => files.map((file) => `ajv compile --spec=draft2020 -s ${file}`),
-  'data/json/schemas/{guideline,check}.json': (files) => files.map((file) => `ajv compile --spec=draft2020 -s ${file} -r data/json/schemas/common.json`),
+  'data/json/schemas/{guideline,check,faq}.json': (files) => files.map((file) => `ajv compile --spec=draft2020 -s ${file} -r data/json/schemas/common.json`),
   'data/yaml/gl/**/*.yaml': (files) => files.map((file) => `ajv validate --spec=draft2020 -s data/json/schemas/guideline.json -r data/json/schemas/common.json -d ${file}`),
   'data/yaml/checks/**/*.yaml': (files) => files.map((file) => `ajv validate --spec=draft2020 -s data/json/schemas/check.json -r data/json/schemas/common.json -d ${file}`),
+  'data/yaml/faq/**/*.yaml': (files) => files.map((file) => `ajv validate --spec=draft2020 -s data/json/schemas/faq.json -r data/json/schemas/common.json -d ${file}`),
 }
