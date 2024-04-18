0d0694080973879c991326041c1e70a1e4978e40
Masafumi NAKANE <max@freee.co.jp>
2024-04-18 15:14:05 +0900

    ページの最初のh1、section.permalinkの最初のh2-h6にコピーリンクボタンを表示するように変更

--- ja/source/_static/copylink.js
+++ ja/source/_static/copylink.js
@@ -1,17 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   const buttonLabel = 'リンクをコピー';
-  const sections = document.querySelectorAll('section.permalink');
+  function appendCopyButton(element, textToCopy) {
+    const button = document.createElement('button');
+    button.textContent = buttonLabel;
+    button.className = 'copy-link';
+    button.onclick = function () {
+      navigator.clipboard.writeText(textToCopy);
+    };
+    element.appendChild(button);
+  }
 
+  const firstH1 = document.querySelector('h1');
+  if (firstH1) {
+    appendCopyButton(firstH1, location.href);
+  }
+
+  const sections = document.querySelectorAll('section.permalink');
   sections.forEach(section => {
-    const header = section.querySelector('h2');
+    const header = section.querySelector('h2, h3, h4, h5, h6');
     if (header) {
-      const button = document.createElement('button');
-      button.textContent = buttonLabel;
-      button.className = 'copy-link';
-      button.onclick = function () {
-        navigator.clipboard.writeText(location.origin + location.pathname + '#' + section.id);
-      };
-      header.appendChild(button);
+      const urlToCopy = location.origin + location.pathname + '#' + section.id;
+      appendCopyButton(header, urlToCopy);  // sectionのIDを含むURLを使用
     }
   });
 });
