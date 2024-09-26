f5de0be3f7447c0905e572e2749dad02ab8ff9ef
Masafumi NAKANE <max@freee.co.jp>
2024-09-26 16:28:22 +0900

    Ver. 202409.0 (2024-09-26) リリース準備

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
-guidelines_version = 'Ver. 202408.0'
+guidelines_version = 'Ver. 202409.0'
 checksheet_version = '5.1.0'
 checksheet_date = '2024-09-26'
-publishedDate = '2024-08-20'
+publishedDate = '2024-09-26'
