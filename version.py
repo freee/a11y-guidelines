9c847fcdc8cac59c68cffe80f0e3e05ef39b106c
Masafumi NAKANE <max@freee.co.jp>
2024-12-16 12:38:50 +0900

    チェックシートV6.0.0の更新情報を追加し、バージョン情報を更新

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 guidelines_version = 'Ver. 202411.0'
-checksheet_version = '5.1.0'
-checksheet_date = '2024-09-26'
+checksheet_version = '6.0.0'
+checksheet_date = '2024-12-16'
 publishedDate = '2024-11-22'
